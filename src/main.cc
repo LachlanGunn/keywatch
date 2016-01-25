2f222364396f41ef2d617f60d0b18de4b2c34d3a
Lachlan Gunn <lachlan@twopif.net>
2016-01-25 11:07:26 +0100

    Added command-line options to keywatchd.

--- src/main.cc
+++ src/main.cc
@@ -5,32 +5,67 @@
 #include <string>
 #include <iostream>
 
+#include <boost/program_options.hpp> // NOLINT
+
 #include "hkp/hkp.h"
 #include "keys/keys.h"
 
 using keywatch::keys::PublicKey;
 using keywatch::keys::UserID;
 
+namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
   keywatch::hkp::HKPInit();
-  
+
+  po::options_description desc("Allozed options");
+  desc.add_options()
+      ("help", "produce help message")
+      ("recipient,r", po::value< std::vector<std::string> >(),
+                      "recipient to look up");
+
+  po::positional_options_description p;
+  p.add("recipient", -1);
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv)
+            .options(desc)
+            .positional(p)
+            .run(), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
+
+  std::cerr << "Preparing to do lookup.\n";
+
   keywatch::hkp::HKPServer server("http://jirk5u4osbsr34t5.onion:11371",
                                   "localhost:9050");
-  std::list<PublicKey> keys = server.GetKeys("lachlan@twopif.net");
 
-  std::list<PublicKey>::const_iterator iterator, end;
-  for (iterator = keys.begin(), end = keys.end();
-       iterator != end;
-       iterator++) {
-    printf("Key: %s\n", (*iterator).identifier().c_str());
+  std::vector<std::string> recipients =
+      vm["recipient"].as< std::vector<std::string> >();
 
-    std::list<UserID>::const_iterator iterator_uid, end_uid;
-    std::list<UserID> uids = iterator->uids();
-    for (iterator_uid = uids.begin(), end_uid = uids.end();
-         iterator_uid != end_uid;
-         iterator_uid++) {
-      printf("    UID: %s\n", iterator_uid->identifier().c_str());
+  for (std::vector<std::string>::const_iterator i = recipients.begin();
+       i != recipients.end(); i++) {
+    std::cerr << "Looking up " << *i << "\n";
+
+    std::list<PublicKey> keys = server.GetKeys(*i);
+
+    std::list<PublicKey>::const_iterator iterator, end;
+    for (iterator = keys.begin(), end = keys.end();
+         iterator != end;
+         iterator++) {
+      printf("Key: %s\n", (*iterator).identifier().c_str());
+
+      std::list<UserID>::const_iterator iterator_uid, end_uid;
+      std::list<UserID> uids = iterator->uids();
+      for (iterator_uid = uids.begin(), end_uid = uids.end();
+           iterator_uid != end_uid;
+           iterator_uid++) {
+        printf("    UID: %s\n", iterator_uid->identifier().c_str());
+      }
     }
   }
 
